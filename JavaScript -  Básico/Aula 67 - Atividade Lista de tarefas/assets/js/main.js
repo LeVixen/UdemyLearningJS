263d70da52a6b2f0db24b7d210d6f2cb50d1ca1c
Vinicius Martins <viniciusmartinsopbr.02@gmail.com>
2023-01-24 10:08:52 -0300

    att

--- JavaScript -  Básico/Aula 67 - Atividade Lista de tarefas/assets/js/main.js
+++ JavaScript -  Básico/Aula 67 - Atividade Lista de tarefas/assets/js/main.js
@@ -12,16 +12,14 @@ function addTask() {
    if (taskValue != 0) {
       let newItem = `<li id="${contador}">${taskValue}<button onclick="removeTask(${contador})">Apagar</button></li>`;
       listTask.innerHTML += newItem;
-      window.localStorage.setItem(contador, newItem)
+      localStorage.setItem(contador, newItem)
    }
 };
 
 function loadTask() {
-   let storage = window.localStorage;
-   for (let i = 0; i <= storage.length; i++) {
+   for (let i = 0; i <= localStorage.length; i++) {
       let itens = storage.getItem(i);
       if (itens !== null) {
-         console.log(itens);
          listTask.innerHTML += itens;
       }
    }
@@ -29,5 +27,5 @@ function loadTask() {
 
 function removeTask(id) {
    document.getElementById(id).remove();
-   window.localStorage.removeItem(id)
+   localStorage.removeItem(id)
 };
