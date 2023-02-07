67e5a6eb6486e5ce9477cf1e60813971e53a4795
Vinicius Martins <74664750+LeVixen@users.noreply.github.com>
2023-02-07 17:27:33 -0300

    doc: Aprendeno Esco Lexico

---
+++ JavaScript -  Básico/Aula 68 - Funções Avançadas/escopo lexico.js
@@ -0,0 +1,14 @@
+const nome =  'Luiz';
+
+function falaNome(){
+   console.log(nome);
+}
+falaNome();
+
+
+// ? função sabe onde foi criade e quais eram seus vizinhos;
+
+function usaFalaNome() {
+   const nome = 'Otávio';
+   falaNome();
+}
